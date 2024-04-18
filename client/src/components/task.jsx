355020dd160c6f6a13b74ad4bc739fb6acf6c8bf
Sunny Anter <sunny@veskim.com>
2024-04-18 22:05:40 +0100

    fix: fixed spacing and colors in task buttons

--- client/src/components/task.jsx
+++ client/src/components/task.jsx
@@ -34,7 +34,7 @@ import {
 } from "@/components/ui/tooltip"
 function Task({task, handleProgress,handleDelete}) {
   const progress = task.progress;
-  const date = moment(task.due_date).format('MMM Do, YY');
+  const date = moment(task.due_date).format('MMM Do, YYYY');
   return (
     <>
       <Card className="flex flex-row gap-8 w-[940px] h-[66px] justify-start items-center">
@@ -43,8 +43,7 @@ function Task({task, handleProgress,handleDelete}) {
         </CardHeader>
         <CardDescription className="flex flex-row gap-2 justify-start items-center">
           <CardTitle className="w-23 flex justify-center items-center">Due Date</CardTitle>
-          <Input disabled type="text" value={date} className="w-28" />
-          <CalendarIcon className="ml-[-3px] h-7 w-7 opacity-50 mr-3" />
+          <Input disabled type="text" value={date} className="w-32 mr-3" />
           {
             (progress === 0) ?
               <Badge className="h-8 w-24 bg-cyan-500 hover:bg-cyan-600 flex justify-center text-cyan-50">Not Started</Badge>
@@ -56,10 +55,10 @@ function Task({task, handleProgress,handleDelete}) {
 
           }
         </CardDescription>
-        <div className="flex gap-2 items-center">
+        <div className="flex justify-between items-center ml-[-15px] w-[340px]">
         <Select>
-          <SelectTrigger className="w-28 h-8 ml-[-10px]">
-            <SelectValue placeholder="Assignees" />
+          <SelectTrigger className="w-28 h-8">
+            <SelectValue placeholder="Assignees"/>
           </SelectTrigger>
           <SelectContent>
             <SelectGroup>
@@ -78,7 +77,7 @@ function Task({task, handleProgress,handleDelete}) {
                 You can track the progress of your task in this segment.
               </SheetDescription>
             </SheetHeader>
-            <div className="h-full w-full mt-6 border-2 rounded-md border-slate-500">
+            <div className="h-full w-full mt-6 border-2 rounded-md border-slate-400">
              
             </div>
           </SheetContent>
@@ -86,7 +85,7 @@ function Task({task, handleProgress,handleDelete}) {
           <TooltipProvider>
             <Tooltip>
               <TooltipTrigger asChild>
-                <Button variant="outline" className="flex h-7 w-7 items-center justify-center rounded-lg text-muted-foreground transition-cozlors hover:text-foreground md:h-7 md:w-7 p-1"><CirclePlay size={24} /></Button>
+                <Button variant="outline" className="flex h-7 w-7 items-center justify-center rounded-lg text-muted-foreground transition-cozlors hover:text-foreground md:h-8 md:w-8 p-1"><CirclePlay size={24}/></Button>
               </TooltipTrigger>
               <TooltipContent>
                 <p>Start Timer</p>
@@ -96,7 +95,7 @@ function Task({task, handleProgress,handleDelete}) {
           <TooltipProvider>
             <Tooltip>
               <TooltipTrigger asChild>
-                <Button onClick={() => handleProgress(task._id, true)} variant="outline" className="flex h-7 w-7 items-center justify-center rounded-lg text-muted-foreground transition-cozlors hover:text-foreground md:h-7 md:w-7 p-1"><Check size={24} /></Button>
+                <Button onClick={() => handleProgress(task._id, true)} variant="outline" className="flex h-7 w-7 items-center justify-center rounded-lg text-muted-foreground transition-cozlors hover:text-foreground md:h-8 md:w-8 p-1 border-2 border-teal-500"><Check size={24} color="green"/></Button>
               </TooltipTrigger>
               <TooltipContent>
                 <p>Task Completed</p>
@@ -106,7 +105,7 @@ function Task({task, handleProgress,handleDelete}) {
           <TooltipProvider>
             <Tooltip>
               <TooltipTrigger asChild>
-                <Button onClick={() => handleDelete(task._id)} variant="outline" className="flex h-7 w-7 items-center justify-center rounded-lg text-muted-foreground transition-cozlors hover:text-foreground md:h-7 md:w-7 p-1"><Trash2 size={24} /></Button>
+                <Button onClick={() => handleDelete(task._id)} variant="outline" className="flex h-7 w-7 items-center justify-center rounded-lg text-muted-foreground transition-cozlors hover:text-foreground md:h-8 md:w-8 p-1 bg-slate-800 hover:bg-slate-900"><Trash2 size={24} color="white" /></Button>
               </TooltipTrigger>
               <TooltipContent>
                 <p>Delete Task</p>
